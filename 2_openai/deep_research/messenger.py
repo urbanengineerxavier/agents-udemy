4ad50a1e45b728dc110cfa753e3b508278ee0575
Prashanth Xavier <xavier211192@gmail.com>
2026-07-21 23:40:10 +0200

    Push

--- 2_openai/deep_research/messenger.py
+++ 2_openai/deep_research/messenger.py
@@ -11,17 +11,18 @@ EMAIL_SMTP_SERVER = os.getenv("EMAIL_SMTP_SERVER")
 EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")
 
 def send_email(subject, text_body, html_body):
-    msg = EmailMessage()
-    msg["From"] = EMAIL_ADDRESS
-    msg["To"] = EMAIL_ADDRESS
-    msg["Subject"] = subject
-    msg.set_content(text_body)
-    msg.add_alternative(html_body, subtype="html")
+    print(f"Subject: {subject}\n\n{text_body}")
+    # msg = EmailMessage()
+    # msg["From"] = EMAIL_ADDRESS
+    # msg["To"] = EMAIL_ADDRESS
+    # msg["Subject"] = subject
+    # msg.set_content(text_body)
+    # msg.add_alternative(html_body, subtype="html")
 
-    with smtplib.SMTP(EMAIL_SMTP_SERVER, 587) as server:
-        server.starttls()
-        server.login(EMAIL_ADDRESS, EMAIL_APP_PASSWORD)
-        server.send_message(msg)
+    # with smtplib.SMTP(EMAIL_SMTP_SERVER, 587) as server:
+    #     server.starttls()
+    #     server.login(EMAIL_ADDRESS, EMAIL_APP_PASSWORD)
+    #     server.send_message(msg)
 
 
 pushover_user = os.getenv("PUSHOVER_USER")
